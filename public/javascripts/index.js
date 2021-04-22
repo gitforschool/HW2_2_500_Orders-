d89587ae8df580fce91cef487f60abe3af4d2660
dylanyork90 <dylanyork90@outlook>
2021-04-22 16:49:14 -0700

    first button is done

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,84 +5,94 @@ function SalesOrder(pStoreID, pSalesPersonID, pCdID, pPricePaid) {
     this.pricepaid = pPricePaid;
   }
   var ClientNotes = [];  // our local copy of the cloud data
-
+  let validCD = [123456, 123654, 321456, 321654, 654123, 654321, 543216, 354126, 621453,623451]
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    document.getElementById("submit").addEventListener("click", function () {
-        var tStoreID = document.getElementById("storeID").value;
-        var tSalesPersonID = document.getElementById("salesPersonID").value;
-        var tCdID = document.getElementById("cdID").value;
-        var tPricePaid = document.getElementById("pricePaid")
-        var oneSalesOrder = new SalesOrder(tStoreID, tSalesPersonID, tCdID, tPricePaid);
-
-        $.ajax({
-            url: '/NewSalesOrder' ,
-            method: 'POST',
-            dataType: 'json',
-            contentType: 'application/json',
-            data: JSON.stringify(oneSalesOrder),
-            success: function (result) {
-                console.log("Added new sales order")
-            }
-
-        });
-    });
-
     document.getElementById("get").addEventListener("click", function () {
-        updateList()
+        let x = getRandomInt(1,24)
+        document.getElementById("StoreID").value = x;
+        document.getElementById("SalesPersonID").value = GetStoreID(x);
+        document.getElementById("CdID").value = validCD[getRandomInt(0,9)];
+        document.getElementById("PricePaid").value = getRandomInt(5,15);
+         
+        
     });
-    
-    var idToFind = ""; // using the same value from the find operation for the modify
-    // find one to modify
-    document.getElementById("find").addEventListener("click", function () {
-        var tAssignment = document.getElementById("modSalesOrder").value;
-         idToFind = "";
-        for(i=0; i< ClientNotes.length; i++){
-            if(ClientNotes[i].storeID === tStoreID) {
-                idToFind = ClientNotes[i]._id;
-           }
+    function GetStoreID(which){
+        let storeList = [98053, 98007, 98077, 98055, 98011, 98046 ]
+        let pointer = 0;
+        while(which > 4)
+        {
+            pointer++;
+            which = which - 4;
         }
-        console.log(idToFind);
+        return storeList[pointer];
+    };
+
+    //document.getElementById("get").addEventListener("click", function () {
+    //    updateList()
+    //});
+    
+    // var idToFind = ""; // using the same value from the find operation for the modify
+    // // find one to modify
+    // document.getElementById("find").addEventListener("click", function () {
+    //     var tAssignment = document.getElementById("modSalesOrder").value;
+    //      idToFind = "";
+    //     for(i=0; i< ClientNotes.length; i++){
+    //         if(ClientNotes[i].storeID === tStoreID) {
+    //             idToFind = ClientNotes[i]._id;
+    //        }
+    //     }
+    //     console.log(idToFind);
  
-        $.get("/FindSalesOrder/"+ idToFind, function(data, status){ 
-            console.log(data[0].assignment);
-            document.getElementById("mStoreID").value = data[0].storeID;
-            document.getElementById("mSalePersonID").value= data[0].tSalesPersonID;
-            document.getElementById("mGrade").value = data[0].grade;
-            document.getElementById("mPricePaid").value = data[0].completed;
+    //     $.get("/FindSalesOrder/"+ idToFind, function(data, status){ 
+    //         console.log(data[0].assignment);
+    //         document.getElementById("mStoreID").value = data[0].storeID;
+    //         document.getElementById("mSalePersonID").value= data[0].tSalesPersonID;
+    //         document.getElementById("mGrade").value = data[0].grade;
+    //         document.getElementById("mPricePaid").value = data[0].completed;
            
 
-        });
-    });
+    //     });
+    // });
 
     // get the server data into the local array
-    updateList();
+    //updateList();
 
 });
 
 
-function updateList() {
-var ul = document.getElementById('listUl');
-ul.innerHTML = "";  // clears existing list so we don't duplicate old ones
+// function updateList() {
+// var ul = document.getElementById('listUl');
+// ul.innerHTML = "";  // clears existing list so we don't duplicate old ones
 
-//var ul = document.createElement('ul')
+// //var ul = document.createElement('ul')
 
-$.get("/SalesOrder", function(data, status){  // AJAX get
-    ClientNotes = data;  // put the returned server json data into our local array
+// $.get("/SalesOrder", function(data, status){  // AJAX get
+//     ClientNotes = data;  // put the returned server json data into our local array
 
-    // sort array by one property
-    ClientNotes.sort(compare);  // see compare method below
-    console.log(data);
-    //listDiv.appendChild(ul);
-    ClientNotes.forEach(ProcessOneSalesOrder); // build one li for each item in array
-    function ProcessOneSalesOrder(item, index) {
-        var li = document.createElement('li');
-        ul.appendChild(li);
+//     // sort array by one property
+//     ClientNotes.sort(compare);  // see compare method below
+//     console.log(data);
+//     //listDiv.appendChild(ul);
+//     ClientNotes.forEach(ProcessOneSalesOrder); // build one li for each item in array
+//     function ProcessOneSalesOrder(item, index) {
+//         var li = document.createElement('li');
+//         ul.appendChild(li);
 
-        li.innerHTML=li.innerHTML + index + ": " + " GradedAssignment: " + item.storeID + "  " + item.salesPersonID + ":  " + item.cdID + " Done? "+ item.pricepaid;
-    }
-});
+//         li.innerHTML=li.innerHTML + "StoreID: " + item.storeID + "  " + "SalesPersonID:" + " " + item.salesPersonID + " CdID: " + item.cdID + " PricePaid: "+ item.pricepaid;
+//     }
+ //});
+//}
+
+
+function getRandomInt(min, max){
+    return Math.floor(Math.random() * (max - min)) + min;
+
 }
+
+
+
+
 
 function compare(a,b) {
     if (a.completed == false && b.completed== true) {
